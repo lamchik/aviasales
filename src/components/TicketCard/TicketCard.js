b8483b5414408dd53e64255649b3dec86fc41b91
lamchik <lamaralanchava@gmail.com>
2021-07-21 15:42:09 +0300

    add carrier's logo

--- src/components/TicketCard/TicketCard.js
+++ src/components/TicketCard/TicketCard.js
@@ -1,10 +1,44 @@
 import './TicketCard.css'
-import transporterLogo from '../../images/transporterLogo.png'
+import s7 from '../../images/s7.png'
+import ey from '../../images/ey.png'
+import fv from '../../images/fv.jpeg'
+import ek from '../../images/ek.png'
+import tg from '../../images/tg.png'
+import su from '../../images/su.png'
+import mh from '../../images/mh.png'
 import Segments from "../Segments/Segments";
 
 
 function TicketCard(props) {
   const {ticket} = props
+  function showCarriersLogo(carrier) {
+    if (carrier === 'S7') {
+      return s7
+    }
+   else if (carrier === 'EY') {
+      return ey
+    }
+   else if (carrier === 'FV') {
+      return fv
+    }
+   else if (carrier === 'EK') {
+      return ek
+    }
+   else if (carrier === 'TG') {
+      return tg
+    }
+   else if (carrier === 'SU') {
+      return su
+    }
+   else if (carrier === 'MH') {
+      return mh
+    }
+   else {
+     return "перевозчик"
+    }
+
+  }
+  console.log(ticket.carrier)
 
   const convertedPrice = ticket.price.toLocaleString('ru-RU')
   const segments = ticket.segments.map((segment) => (
@@ -12,18 +46,18 @@ function TicketCard(props) {
       origin={segment.origin}
       destination={segment.destination}
       timeOfDeparture={segment.date}
-      // arrivalTime={555}
       duration={segment.duration}
       stops={segment.stops}
       countStops={2}
     />
   ))
-  
+
   return (
     <div className='ticket'>
       <div className='ticket__header'>
         <p className='ticket__header-price'>{`${convertedPrice} Р`}</p>
-        <img className='ticket__header-transporter' alt='логотип компании-перевозчика' src={transporterLogo}/>
+        <img className='ticket__header-transporter' alt='логотип компании-перевозчика'
+             src={showCarriersLogo(ticket.carrier)}/>
       </div>
       <div>{segments}</div>
     </div>
